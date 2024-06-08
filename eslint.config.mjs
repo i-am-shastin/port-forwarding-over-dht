93a46584bfe41acc09d807704ac95ed01d5ccdf2
Igor Shastin <igor.shastin@kofax.com>
2024-06-08 23:30:49 +0800

    style: Add eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,40 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import eslintPluginImport from 'eslint-plugin-import';
+import tseslint from 'typescript-eslint';
 
 
-export default [
-    { languageOptions: { globals: globals.node } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        rules: {
-            "no-unused-vars": "warn",
-            "no-undef": "warn"
-        },
-        ignores: ["**/*.d.ts"]
+export default tseslint.config({
+    plugins: {
+        'import': eslintPluginImport
+    },
+    files: ['src/**/*.ts', '*.mjs'],
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommended
+    ],
+    rules: {
+        'semi': ['error', 'always', { 'omitLastInOneLineClassBody': true, 'omitLastInOneLineBlock': true }],
+        'quotes': ['error', 'single', { 'avoidEscape': true }],
+        'arrow-parens': ['error', 'always'],
+        'max-len': ['error', { 'code': 160, 'tabWidth': 4 }],
+        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        'sort-imports': ['error', { 'ignoreDeclarationSort': true }],
+        'import/newline-after-import': ['error', { 'count': 2 }],
+        'import/order': [
+            'error',
+            {
+                'newlines-between': 'always',
+                'groups': ['builtin', 'external', 'parent', ['internal', 'sibling', 'index', 'object', 'type']],
+                'alphabetize': { 'order': 'asc', 'caseInsensitive': true },
+            }
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        'no-multiple-empty-lines': ['error', { 'max': 2, 'maxBOF': 0, 'maxEOF': 0 }],
+        'no-trailing-spaces': 'error',
+        'eol-last': ['error', 'always']
+    },
+    settings: {
+        'import/internal-regex': '~'
     }
-];
+});
