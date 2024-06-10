34268de5cbfdfa1620778de0f3c37e82de55ed9e
Igor Shastin <igor.shastin@kofax.com>
2024-06-10 11:49:56 +0800

    feat: Easy configuration mode

--- src/services/node/factory.ts
+++ src/services/node/factory.ts
@@ -6,6 +6,7 @@ import { TCPClient, UDPClient } from '~services/node/client';
 import { NodeInstance } from '~services/node/instance';
 import { TCPServer, UDPServer } from '~services/node/server';
 import { Configuration, Node } from '~types';
+import { Console } from '~utils/console';
 
 
 export class NodeFactory {
@@ -18,16 +19,25 @@ export class NodeFactory {
 
     async run() {
         await this.dht.ready();
-        const nodes = this.config.nodes.map(
-            (node) => {
-                const instance = this.config.server
-                    ? this.createServer(node)
-                    : this.createClient(node);
 
-                return instance.init(this.dht, this.keychain);
-            }
-        );
-        await Promise.allSettled(nodes);
+        let nodes = this.config.nodes;
+        if (!this.config.server && this.config.easy) {
+            nodes = await this.readConfigurationFromServer();
+        }
+
+        const instances = nodes.map((node) => {
+            const instance = this.config.server
+                ? this.createServer(node)
+                : this.createClient(node);
+
+            return instance.init(this.dht, this.keychain);
+        });
+
+        if (this.config.server && this.config.easy) {
+            await this.createConfigurationServer();
+        }
+
+        await Promise.allSettled(instances);
     }
 
     private createServer(node: Node): NodeInstance {
@@ -36,8 +46,6 @@ export class NodeFactory {
                 return new UDPServer(node);
             case ProtocolType.TCP:
                 return new TCPServer(node);
-            default:
-                throw new RangeError(`Unknown server protocol: "${node.protocol}"`);
         }
     }
 
@@ -47,8 +55,25 @@ export class NodeFactory {
                 return new UDPClient(node);
             case ProtocolType.TCP:
                 return new TCPClient(node);
-            default:
-                throw new RangeError(`Unknown client protocol: "${node.protocol}"`);
         }
+    }
+
+    private async createConfigurationServer() {
+        const server = this.dht.createServer({ reusableSocket: true }, (socket) => {
+            Console.info('New client connected, sending node configuration');
+            socket.write(JSON.stringify(this.config.nodes));
+        });
+        await server.listen(this.keychain.baseKeyPair);
+    }
+
+    private async readConfigurationFromServer(): Promise<Node[]> {
+        return new Promise((resolve) => {
+            const socket = this.dht.connect(this.keychain.baseKeyPair.publicKey, { reusableSocket: true });
+            socket.on('message', (buffer: string) => {
+                Console.info('Received node configuration from server');
+                const nodes = JSON.parse(buffer) as Node[];
+                resolve(nodes);
+            });
+        });
     }
 }
